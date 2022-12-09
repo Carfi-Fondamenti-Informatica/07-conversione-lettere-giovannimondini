e0975eeb1e5f1bda5cd66ed5078ce7234e39b109
giovannimondini <117928503+giovannimondini@users.noreply.github.com>
2022-12-09 07:09:40 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,26 @@
+#include "lib.h"
+bool funzione(char a,char &t)
+{
+    int b;
+    b= (int) a;
+    if(b<=122&&b>=97)
+    {
+        b=b-32;
+        t= (char) b;
+        return true;
+    }
+    else
+    {
+        if(b<=90&&b>=65)
+        {
+            b=b+32;
+            t= (char) b;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
+}
